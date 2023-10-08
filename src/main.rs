a0469151c7fb2c9e5a30c665f5a4f1555847d917
Nick Baratashvili <nikobar3005@gmail.com>
2023-10-08 23:28:40 +0400

    not much

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,10 @@
+use std::fs::File;
+use std::io::Read;
+
 mod train1;
 mod train2;
 
-fn get_count(string: &str) -> usize {
-    let mut vowels_count: usize = 0;
-
-    for c in string.chars() {
-        if c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' {
-            vowels_count += 1;
-        }
-    }
-    
-    vowels_count
-}
-
 fn main() {
-    println!("{:#?}", get_count("abracadabra"));
+    
 }
   
